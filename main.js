04319e99d8a691a8aad7fe196f10cbe9b6561712
Dang Xuan Khanh <khanhdang3152@gmail.com>
2024-04-26 22:26:41 +0700

    mucsic on play

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
 const $=document.querySelector.bind(document);
 const $$=document.querySelectorAll.bind(document);
+
 let btnPlay=$(".btn-play");
-let currentPlaying=$("audio");
+let btnNext=$(".btn-next");
+let btnPrev=$(".btn-prev"); 
+
+
+let songNameHeading=$(".song-name-playing");
+let cdThumb=$(".cd-thumb");
+let audio=$("audio");
+let progress=$(".progress");
+
 const app={
     isPlaying:false,
     songs:[
@@ -37,31 +46,113 @@ const app={
             image:"./assets/despacito.png"
         }
     ],
-    playSong:function()
+    currentSong:"",
+    currentIndex:0,
+    handleEvents:function()
     {
+        // khi bam nut play
         btnPlay.onclick=function()
         {
-            if(this.isPlaying==false)
+            if(app.isPlaying==false)
             {
-                currentPlaying.play();
-                this.isPlaying=true;
-                btnPlay.innerHTML=`<i class="fa-solid fa-pause"></i>`;
+                audio.play();
             }
             else{
-                currentPlaying.pause();
-                this.isPlaying=false;
-                btnPlay.innerHTML=`<i class="fa-solid fa-play"></i>`;
+                audio.pause();
+                
             }
-        }  
+        }
+        // Khi play
+        audio.onplay=function(){
+            app.isPlaying=true;
+            btnPlay.innerHTML=`<i class="fa-solid fa-pause"></i>`;
+           
+        }
+      
+        // khi pause
+        audio.onpause=function(){
+            app.isPlaying=false;
+            btnPlay.innerHTML=`<i class="fa-solid fa-play"></i>`;
+        }
+          // Khi time update
+        audio.ontimeupdate=function(){
+            // set thoi gian cho progress
+            let currentTime=audio.currentTime;
+            let duration=audio.duration;
+            progress.value=Math.floor((currentTime/duration)*100);
+        }    
+        //Khi tua audio
+        progress.onchange=function(){
+            audio.currentTime=(progress.value*audio.duration)/100;
+        }
+        // Khi click vao next
+        btnNext.onclick=function(){
+            app.nextSong();
+            app.loadCurrentSong();
+            //set lai progress
+            progress.value=0;
+            audio.play();
+        }
+        //Khi click vao prev
+        btnPrev.onclick=function(){
+            app.prevSong();
+            app.loadCurrentSong();
+            //set lai progress
+            progress.value=0;
+            audio.play();
+        }
+        //Chuyen bai
+        for(var item of $$(".song"))
+        {
+            console.log(item.getAttribute("data-id")+"Sss");
+            item.onclick=function(e)
+            {
+                // Bo ac class active
+                for(var song of $$(".song"))
+                {
+                    song.classList.remove("active");
+                }
+                let songElement=e.target.closest(".song");
+                songElement.classList.add("active");
+                console.log(songElement);
+                app.currentIndex=songElement.getAttribute("data-id");
+                app.loadCurrentSong();
+                //set lai progress
+                progress.value=0;
+                audio.play();
+            }
+        }
+        
+    },
+    loadCurrentSong:function(){
+        app.currentSong=app.songs[app.currentIndex];
+        audio.setAttribute("src",`${app.currentSong.path}`);
+            
+        songNameHeading.innerHTML=`${app.currentSong.name}`
+        cdThumb.src=app.currentSong.image;
+    },
+    nextSong:function(){
+        this.currentIndex++;
+        if(this.currentIndex>this.songs.length-1){
+            this.currentIndex=0;
+        }
+        
+    },
+    prevSong:function(){
+        this.currentIndex--;
+        if(this.currentIndex<0){
+            this.currentIndex=0;
+        }
         
     },
     render:function()
     {
         html=``;
+        let i=0;
         for(var item of this.songs)
         {
             html+=`
-            <div class="song">
+            <div class="song" data-id="${i}">
                 <div class="thumb" style="background-image: url('${item.image}')">
                 </div>
                 <div class="body">
@@ -77,13 +168,15 @@ const app={
                 </div>
             </div>
             `
+            i++;
         }
         $(".playlist").innerHTML=html;
     },
 
     start:function(){
         this.render();
-        this.playSong();
+        this.loadCurrentSong();
+        this.handleEvents();
     }
 }
 
